6046ff6303e9ed04c9c6937784d8d8f82a09354c
Victor <victor.shevchuk.96@gmail.com>
2022-12-18 17:34:43 +0200

    covered movie card with tests

--- client/src/components/MovieCard/MovieCard.test.js
+++ client/src/components/MovieCard/MovieCard.test.js
@@ -1,23 +1,29 @@
-import { render, cleanup, screen, fireEvent } from "@testing-library/react";
+import { cleanup, screen } from "@testing-library/react";
 import { MemoryRouter as Router } from "react-router-dom";
+
+import { renderWithProviders } from "test/test-utils";
 
 import { MovieCard } from "components";
 
-afterEach(cleanup);
+afterEach(() => {
+  cleanup();
+  console.error.mockClear();
+});
 
 console.error = jest.fn();
 
 describe("<MovieCard />", () => {
   test("without movie", () => {
-    // render(
-    //   <Router>
-    //     <MovieCard />
-    //   </Router>
-    // );
-    // expect(console.error).toHaveBeenCalledTimes(1);
+    renderWithProviders(
+      <Router>
+        <MovieCard />
+      </Router>
+    );
+
+    expect(console.error).toHaveBeenCalled();
   });
 
-  test("with movie", () => {
+  test("with movie", async () => {
     const movie = {
       id: 337167,
       genres: ["Drama", "Romance", "Crime"],
@@ -31,10 +37,25 @@ describe("<MovieCard />", () => {
       releaseDate: "2018-02-07",
     };
 
-    // render(
-    //   <Router>
-    //     <MovieCard />
-    //   </Router>
-    // );
+    renderWithProviders(
+      <Router>
+        <MovieCard
+          id={movie.id}
+          genres={movie.genres}
+          title={movie.title}
+          runtime={movie.runtime}
+          overview={movie.overview}
+          voteAverage={movie.voteAverage}
+          posterPath={movie.posterPath}
+          releaseDate={movie.releaseDate}
+        />
+      </Router>
+    );
+
+    expect(console.error).not.toHaveBeenCalled();
+
+    const title = await screen.findByText(movie.title);
+
+    expect(title.textContent).toBe(movie.title);
   });
 });
